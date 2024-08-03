d3d2f3373dd141f8d57eb594512374f86fdf4a30
Md Azizul <khanazizul1199@gmail.com>
2024-08-03 12:18:37 +0600

    added server api

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000",
+	baseURL: "https://mfs-server-delta.vercel.app",
 });
 const useAxiosSecure = () => {
 	const navigate = useNavigate();
